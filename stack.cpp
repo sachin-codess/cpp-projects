9314dc244295cc07f898ced9cab50468851b342d
sachin-codess <sachinydv843@gmail.com>
2026-04-04 23:54:12 -0500

    Create stack.cpp

---
+++ stack.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+class Stack {
+private:
+    Node* top;
+public:
+    Stack() { top = NULL; }
+
+    void push(int value) {
+        Node* newNode = new Node();
+        newNode->data = value;
+        newNode->next = top;
+        top = newNode;
+    }
+
+    void pop() {
+        if (top == NULL) {
+            cout << "Stack is empty\n";
+            return;
+        }
+        Node* temp = top;
+        top = top->next;
+        delete temp;
+    }
+
+    void display() {
+        Node* temp = top;
+        while (temp != NULL) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    Stack s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.display();
+    s.pop();
+    s.display();
+    return 0;
+}
